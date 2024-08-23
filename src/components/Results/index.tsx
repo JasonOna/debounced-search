73b0fd04a455d35d396342d796a14b440b7a3f51
Jason Ona <onajjb@gmail.com>
2024-08-23 10:04:07 +1000

    Add default exports

--- src/components/Results/index.tsx
+++ src/components/Results/index.tsx
@@ -5,15 +5,17 @@ type Result = {
   image: string;
 };
 
-export const Results = ({ results }: { results: Result[] }) => {
+export default function Results({ results }: { results: Result[] }) {
   return (
     <div className="flex flex-wrap max-h-96 overflow-auto">
       {results.map((result) => {
         return (
           <div key={result.id} className="w-6/12 grid grid-cols-3">
-            <img src={result.image} className="h-32" />
+            <section className="m-auto">
+              <img src={result.image} className="max-h-32 max-w-32 w-full" />
+            </section>
             <section className="col-span-2 flex flex-col">
-              <h1 className="text-xl uppercase">{result.title}</h1>
+              <h1 className="text-xl uppercase mb-1">{result.title}</h1>
               <h2 className="text-lg text-slate-500">
                 {parsePrice(result.price)}
               </h2>
@@ -23,7 +25,7 @@ export const Results = ({ results }: { results: Result[] }) => {
       })}
     </div>
   );
-};
+}
 
 // TODO: refactor if more complex display is needed
 const parsePrice = (price: number) => {
